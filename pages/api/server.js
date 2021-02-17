8f1d047a2dfff48860184282cb6e43488421c6f2
Lucivaldo Castro <lucivaldocb@gmail.com>
2021-02-17 14:48:55 -0300

    Deletado página desnecessária

--- pages/api/server.js
+++
@@ -1,9 +0,0 @@
-import { obterChamados, obterChamadosPendentes } from '../../lib/help_desk'
-
-export default (req, res) => {
-  obterChamadosPendentes()
-  .then(response => {
-    res.statusCode = 200
-    res.json(response)
-  })
-}
